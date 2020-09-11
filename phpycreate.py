8a46ce9130a49d237e0c4378962d439a7ddfea46
RLogik <r.bitlogik@gmail.com>
2020-09-11 13:13:47 +0200

    more natural handling of escape characters
    strings typed in phpycreate.yml appear verbatim in parameters.tex

--- phpycreate.py
+++ phpycreate.py
@@ -6,7 +6,7 @@
 # AUTHOR: R-Logik, Deutschland. https://github.com/RLogik/phpytex
 # CREATED: 27.07.2020
 # LAST CHANGED: 10.09.2020
-# VERSION: 1·1·3
+# VERSION: 1·1·4
 # NOTES:
 #
 #     Installation:
@@ -41,7 +41,7 @@ from subprocess import Popen;
 from gitignore_parser import parse_gitignore;
 
 console_quiet = False;
-PHPYCREATE_VERSION = '1·1·3';
+PHPYCREATE_VERSION = '1·1·4';
 FILE_EXT_PPTSTRUCT = r'\.phpycreate\.(yml|yaml)';
 FILE_EXT_PPTIGNORE = r'\.phpycreate\.ignore';
 
@@ -608,7 +608,7 @@ def get_dict_value(obj, key: Union[str,List[str]], *keys: Union[str,List[str]], 
 
 def to_python_string(value) -> Union[str, None]:
     if isinstance(value, str):
-        return "'{}'".format(value);
+        return "r'{}'".format(value);
     elif isinstance(value, (int, float, bool, EvalType)) or value is None:
         return str(value);
     elif isinstance(value, list):
